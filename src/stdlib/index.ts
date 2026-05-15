7f367ebfcadacbb450f3967d40d86e59c1bcb81d
Peace Thabiwa <peacethabibinflow@proton.me>
2026-05-15 09:41:58 +0100

    Create index.ts

---
+++ src/stdlib/index.ts
@@ -0,0 +1,25 @@
+export {
+PHI, PI, TAU,
+harmonic,
+phi_r,
+fibonacci,
+intAngle,
+extAngle,
+distortionScale,
+clamp,
+lerp,
+normalize
+} from './math';
+export {
+orbit,
+distance,
+insideCircle,
+polygonVertices,
+insidePolygon,
+exteriorAngleVectors
+} from './spatial';
+export {
+hueFromAngle,
+distortionColor,
+decay
+} from './color';
