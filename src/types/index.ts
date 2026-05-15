e400f125eba2f7723092525e20ddb5ece58cb4f6
Peace Thabiwa <peacethabibinflow@proton.me>
2026-05-15 09:26:37 +0100

    Export types from ws-types and compile-types

---
+++ src/types/index.ts
@@ -0,0 +1,25 @@
+export type {
+WSTarget,
+WSParticleKeyword,
+WSClassifierKeyword,
+WSBuiltinFunction,
+WSOperator,
+WSScalar,
+WSPoint,
+WSExpression,
+WSProperty,
+WSRelation,
+WSConstraint,
+WSClassifier,
+WSBoundary,
+WSEntity,
+WSFetchDeclaration,
+WSNode,
+WSProgram
+} from './ws-types';
+export type {
+CompileOptions,
+CompileResult,
+CompileError,
+CompileWarning
+} from './compile-types';
